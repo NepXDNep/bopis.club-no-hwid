51087818c6c70d6e4039933816271279b9a066fc
Evan <41494790+evaan@users.noreply.github.com>
2021-06-25 16:32:47 -0230

    Update HWIDManager.java

--- src/main/java/me/bopis/king/manager/HWIDManager.java
+++ src/main/java/me/bopis/king/manager/HWIDManager.java
@@ -21,11 +21,5 @@ public class HWIDManager {
     public static List<String> hwids = new ArrayList<>();
 
     public static void hwidCheck() {
-        hwids = URLReader.readURL();
-        boolean isHwidPresent = hwids.contains(SystemUtil.getSystemInfo());
-        if (!isHwidPresent) {
-            DisplayUtil.Display();
-            throw new NoStackTraceThrowable("no");
-        }
     }
 }
